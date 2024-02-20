e39904247342106ac514cf8a42237ab5156fd06c
Israel Bucbinder <8200isra@gmail.com>
2024-02-20 13:30:01 +0200

    validation to "add_client"
    adding validaion to the input query "add_client"
    add @staticmetod to all function class "validation"

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import os
 import sys
-from customer import Customer
-from datetime import datetime, date, strftime
+# from customer import Customer
+from datetime import datetime, date
 import re
 
 class Validation():
@@ -21,6 +21,27 @@ class Validation():
         if check_digit != int(str(id_number)[-1]):
             return False
         return True
+    
+    def check_name(name1, last1, first, last):
+        if name1 == first or name1 == None:
+            name1 = first
+        else:
+            print(f"The name {first} does not match the name on this ID!")
+            choss = input(f"Do you to switch the name {first} to {name1}? y/n")
+            if choss == "y":
+                first = name1
+            else:
+                return
+        if last1 == last or last1 == None:
+            last1 = last
+        else:
+            print(f"The name {last} does not match the name on this ID!")
+            choss = input(f"Do you to switch the name {last} to {last1}? y/n")
+            if choss == "y":
+                last = last1
+            else:
+                return
+        print("the names is correct!")
      
     def valid_phone(phone_num):
         if phone_num[0] != "0":
@@ -59,14 +80,35 @@ class Customer:
         self._date = date
 
     @property
+    def first(self):
+        return self._first
+    
+    @property
+    def last(self):
+        return self._last
+    
+    @property
     def id(self):
         return self._id
     
+    @property
+    def phone(self):
+        return self._phone
+    
+    @property
+    def debt(self):
+        return self._debt
+    
+    @property
+    def date(self):
+        return self._date
+    
+
     def __str__(self):
-        return f"name: {self._first}{self._last} ID:{self._id} phone:{self._phone} debt:{self._debt} date:{self._date}"
+        return f"name: {self.first}{self.last} ID:{self.id} phone:{self.phone} debt:{self.debt} date:{self.date}"
     
     def add_debt(self, new_debt):
-        self._debt += new_debt
+        self.debt += new_debt
 
     def update_date(self, date1, date2):
         if date1 > date2:
@@ -76,18 +118,20 @@ class Customer:
 
 class Customers_list():
     def __init__(self):
-        self.customers = []
+        self.customers:list[Customer] = []
     
-    def add_custemer(self, new_custemer):        
-        id = new_custemer[2]
+    def add_custemer(self, new_customer:list[str]):
+        id = new_customer[2]
         for customer in self.customers:
             if customer.id == id:
-                customer.add_debt(float(new_custemer[4]))
-                customer._date = customer.update_date(new_custemer[5], customer._date)
+                customer.check_name(new_customer[0], new_customer[1], customer.first, customer.last) #שים לב לייצר פונקציית בדיקת שמות
+                customer.add_debt(float(new_customer[4]))
+                customer.date = customer.update_date(new_customer[5], customer.date)
                 break
         else:
-            customer = Customer(*new_custemer)
+            customer = Customer(*new_customer)
             self.customers.append(customer)
+    
     def __str__(self):
         customer_info = [str(customer) for customer in self.customers]
         return "\n".join(customer_info)
@@ -99,7 +143,7 @@ class Customers_list():
 
     def sort_by_debt_mini(self):
         print("in sort")
-        self.customers.sort(key=lambda x: x._debt, reverse=True)
+        self.customers.sort(key=lambda x: x.debt, reverse=True)
         print(customers_list)
 
 
@@ -119,7 +163,7 @@ with open(csv_file, "r") as fd:
         customers_list.add_custemer(file_line_dtb)
 
 
-    
+validation = Validation()
 while True:
     query = input("==> ")
     if query == "quit":
@@ -131,5 +175,17 @@ while True:
         customers_list.sort_by_debt()
     if query == "sort_mini":
         customers_list.sort_by_debt_mini()
-        
-    
+    if query == "add_client":
+        names = ["first_name", "last_name", "id", "phone_number", "debt", "date"]
+        values = [input(f"Enter a {name}: ") or 'Empty No input entered!' for name in names]
+        if not validation.valid_id(values[2]): 
+            print("Error: the id didn't put or not correct!")
+        if not validation.valid_phone(values[3]):
+            print("Error: the phone didn't put or not correct!")
+        if not validation.money_amount(values[4]):
+            print("Error: the debt didn't put or not correct!")
+        if not validation.valid_date(values[5]):
+            print("Error: the date didn't put or not correct!")
+        print(f" thas is the added {values}")
+        customers_list.add_custemer(values)
+            
